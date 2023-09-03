87a1a6cf497c42a304abb994901343d708437391
Joshua-VI-Taylor <josddd0@gmail.com>
2023-09-02 23:23:51 -0400

    Making it so the Graphics Engine can render a triangle

---
+++ GameEngine_0/GameEngine_0/GraphicsEngine.h
@@ -0,0 +1,57 @@
+
+#pragma once
+
+#include <d3d11.h>
+#include "SwapChain.h"
+#include "VertexBuffer.h"
+
+
+#include <d3dcompiler.h>
+
+class SwapChain;
+class DeviceContext;
+class VertexBuffer;
+
+class GraphicsEngine
+{
+public:
+	GraphicsEngine();
+	//Initialize the GraphicsEngine and DirectX 11 Device
+	bool init();
+	//Release all the resources loaded
+	bool release();
+	~GraphicsEngine();
+public:
+	SwapChain* createSwapChain();
+	DeviceContext* getImmediateDeviceContext();
+	VertexBuffer* createVertexBuffer();
+public:
+	bool createShaders();
+	bool setShaders();
+	void getShaderBufferAndSize(void** bytecode, UINT* size);
+public:
+	static GraphicsEngine* get();
+
+private:
+	DeviceContext* m_imm_device_context = nullptr;
+private:
+	ID3D11Device* m_d3d_device = nullptr;
+	D3D_FEATURE_LEVEL m_feature_level;
+private:
+	IDXGIDevice* m_dxgi_device = nullptr;
+	IDXGIAdapter* m_dxgi_adapter = nullptr;
+	IDXGIFactory* m_dxgi_factory = nullptr;
+	ID3D11DeviceContext* m_imm_context = nullptr;
+
+private:
+	ID3DBlob* m_vsblob = nullptr;
+	ID3DBlob* m_psblob = nullptr;
+	ID3D11VertexShader* m_vs = nullptr;
+	ID3D11PixelShader* m_ps = nullptr;
+private:
+	friend class SwapChain;
+	friend class VertexBuffer;
+};
+
+
+
