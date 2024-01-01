e9939699e98108e1cedd9280f716e6447413bae2
Joshua Taylor <74110017+Joshua-VI-Taylor@users.noreply.github.com>
2024-01-01 03:43:58 -0500

    Adding Assesets

--- GameEnine_0/Assests/Shaders/MyMaterial.hlsl
+++ GameEnine_0/Assests/Shaders/MyMaterial.hlsl
@@ -7,76 +7,55 @@ struct VS_INPUT
     float3 binormal : BINORMAL0;
 };
 
-struct VS_OUTPUT
+struct VPS_INOUT
 {
     float4 position : SV_POSITION;
     float2 texcoord : TEXCOORD0;
     float3 normal : NORMAL0;
+    float3 worldPosition : TEXCOORD1;
 };
 
-//Retrive data is c++ version of the constant buffer
+struct LightData
+{
+    float4 color;
+    float4 direction;
+};
+
 cbuffer constant : register(b0)
 {
     row_major float4x4 world;
     row_major float4x4 view;
     row_major float4x4 proj;
+    float4 cameraPosition;
+    LightData light;
 };
 
 
-VS_OUTPUT vsmain(VS_INPUT input)
+
+VPS_INOUT vsmain(VS_INPUT input)
 {
-    VS_OUTPUT output = (VS_OUTPUT) 0;
-		
+    VPS_INOUT output = (VPS_INOUT)0;
+
 	//WORLD SPACE
     output.position = mul(input.position, world);
-	//VIEW SPACE
+    output.worldPosition = output.position.xyz;
+
     output.position = mul(output.position, view);
-	//SCREEN SPACE
     output.position = mul(output.position, proj);
     output.texcoord = input.texcoord;
-    //Computing World Normal
     output.normal = normalize(mul(input.normal, (float3x3) world));
     return output;
 }
 
-//Retrive Earth texture
-Texture2D EarthColor : register(t0);
-sampler EarthColorSampler : register(s0);
-//Retrive EarthSpecular texture
-Texture2D EarthSpecular : register(t1);
-//Since there are multiple ways to sample textures, we need to know how to sazzmple. 
-//This varaible hold the information on how to sample the texture.
-sampler EarthSpecularSampler : register(s1);
-//Retrive Clouds texture
-Texture2D Clouds : register(t2);
-//Since there are multiple ways to sample textures, we need to know how to sample. 
-//This varaible hold the information on how to sample the texture.
-sampler CloudsSampler : register(s2);
-//Retrive EarthNight texture
-Texture2D EarthNight : register(t3);
-//Since there are multiple ways to sample textures, we need to know how to sample. 
-//This varaible hold the information on how to sample the texture.
-sampler EarthNightSampler : register(s3);
-//Custon dat type to store the position, texcoord, normal, and direction_to_camera 
-struct PS_INPUT
-{
-    float4 position : SV_POSITION;
-    float2 texcoord : TEXCOORD0;
-    float3 normal : NORMAL0;
-};
-
-//Retrive Earth texture
 Texture2D Color : register(t0);
 sampler ColorSampler : register(s0);
 
-float4 psmain(PS_INPUT input) : SV_TARGET
+float4 psmain(VPS_INOUT input) : SV_TARGET
 {
-    //Sample texture's data. This will allow us to get the color of every part of the texture and to 
-    //help use assinge that color to the pixels that need it one the screen.
     float4 color = Color.Sample(ColorSampler, input.texcoord);
-    float3 light_direction = normalize(float3(-1, 1, 1));
-    
-    
+    float3 light_direction = normalize(light.direction.xyz);
+    float3 directionToCamera = normalize(input.worldPosition - cameraPosition.xyz);
+
 	//AMBIENT LIGHT
     float ka = 8.5;
     float3 ia = float3(0.09, 0.082, 0.082);
@@ -86,21 +65,21 @@ float4 psmain(PS_INPUT input) : SV_TARGET
 
 	//DIFFUSE LIGHT
     float kd = 0.7;
-    float amount_diffuse_light = max(dot(light_direction.xyz, input.normal), 0.0f);
-    float3 id = float3(1, 1, 1);
+    float amount_diffuse_light = max(dot(light_direction.xyz, input.normal), 0.0);
+    float3 id = light.color.rgb;
     id *= (color.rgb);
     float3 diffuse_light = kd * id * amount_diffuse_light;
 
 	//SPECULAR LIGHT
-    //float ks = 0.0;
-    //float3 is = float3(1.0, 1.0, 1.0);
-    //float3 reflected_light = reflect(light_direction.xyz, input.normal);
-    //float shininess = 30.0;
-    //float amount_specular_light = pow(max(0.0, dot(reflected_light, input.direction_to_camera)), shininess);
+    float ks = 1.0;
+    float3 is = float3(1.0, 1.0, 1.0);
+    float3 reflected_light = reflect(light_direction.xyz, input.normal);
+    float shininess = 30.0;
+    float amount_specular_light = pow(max(0.0, dot(reflected_light, directionToCamera)), shininess);
 
-    //float3 specular_light = ks * amount_specular_light * is;
+    float3 specular_light = ks * amount_specular_light * is;
 
-    float3 final_light = ambient_light + diffuse_light;
+    float3 final_light = ambient_light + diffuse_light + specular_light;
 
     return float4(final_light, 1.0);
 }
